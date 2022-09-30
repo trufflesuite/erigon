55c1fa8a2d64158d0fa2f2a5446e704d35046107
nthpool <contact@nthpool.io>
2022-09-29 19:35:49 -0500

    fix callBundle implementation to match mev-geth

--- cmd/rpcdaemon/commands/eth_block.go
+++ cmd/rpcdaemon/commands/eth_block.go
@@ -2,14 +2,17 @@ package commands
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 	"math/big"
 	"time"
 
+	"github.com/holiman/uint256"
 	"github.com/ledgerwatch/erigon-lib/kv"
 	"github.com/ledgerwatch/erigon/common"
 	"github.com/ledgerwatch/erigon/common/hexutil"
 	"github.com/ledgerwatch/erigon/common/math"
+	"github.com/ledgerwatch/erigon/consensus/misc"
 	"github.com/ledgerwatch/erigon/core"
 	"github.com/ledgerwatch/erigon/core/rawdb"
 	"github.com/ledgerwatch/erigon/core/state"
@@ -23,7 +26,26 @@ import (
 	"golang.org/x/crypto/sha3"
 )
 
-func (api *APIImpl) CallBundle(ctx context.Context, txHashes []common.Hash, stateBlockNumberOrHash rpc.BlockNumberOrHash, timeoutMilliSecondsPtr *int64) (map[string]interface{}, error) {
+// CallBundleArgs represents the arguments for a call.
+type CallBundleArgs struct {
+	Txs                    []hexutil.Bytes       `json:"txs"`
+	BlockNumber            rpc.BlockNumber       `json:"blockNumber"`
+	StateBlockNumberOrHash rpc.BlockNumberOrHash `json:"stateBlockNumber"`
+	Coinbase               *string               `json:"coinbase"`
+	Timestamp              *uint64               `json:"timestamp"`
+	Timeout                *int64                `json:"timeout"`
+	GasLimit               *uint64               `json:"gasLimit"`
+	Difficulty             *big.Int              `json:"difficulty"`
+	BaseFee                *big.Int              `json:"baseFee"`
+}
+
+// Ported from MEV-Geth. CallBundle will simulate a bundle of transactions at the top of a given block
+// number with the state of another (or the same) block. This can be used to
+// simulate future blocks with the current state, or it can be used to simulate
+// a past block.
+// The sender is responsible for signing the transactions and using the correct
+// nonce and ensuring validity
+func (api *APIImpl) CallBundle(ctx context.Context, args CallBundleArgs) (map[string]interface{}, error) {
 	tx, err := api.db.BeginRo(ctx)
 	if err != nil {
 		return nil, err
@@ -35,42 +57,26 @@ func (api *APIImpl) CallBundle(ctx context.Context, txHashes []common.Hash, stat
 		return nil, err
 	}
 
-	if len(txHashes) == 0 {
-		return nil, nil
+	if len(args.Txs) == 0 {
+		return nil, fmt.Errorf("bundle missing txs")
+	}
+
+	if args.BlockNumber == 0 {
+		return nil, fmt.Errorf("bundle missing blockNumber")
 	}
 
 	var txs types.Transactions
 
-	for _, txHash := range txHashes {
-		blockNum, ok, err := api.txnLookup(ctx, tx, txHash)
-		if err != nil {
+	for _, encodedTx := range args.Txs {
+		if tx, err := types.UnmarshalTransactionFromBinary(encodedTx); err != nil {
 			return nil, err
+		} else {
+			txs = append(txs, tx)
 		}
-		if !ok {
-			return nil, nil
-		}
-		block, err := api.blockByNumberWithSenders(tx, blockNum)
-		if err != nil {
-			return nil, err
-		}
-		if block == nil {
-			return nil, nil
-		}
-		var txn types.Transaction
-		for _, transaction := range block.Transactions() {
-			if transaction.Hash() == txHash {
-				txn = transaction
-				break
-			}
-		}
-		if txn == nil {
-			return nil, nil // not error, see https://github.com/ledgerwatch/turbo-geth/issues/1645
-		}
-		txs = append(txs, txn)
 	}
 	defer func(start time.Time) { log.Trace("Executing EVM call finished", "runtime", time.Since(start)) }(time.Now())
 
-	stateBlockNumber, hash, latest, err := rpchelper.GetBlockNumber(stateBlockNumberOrHash, tx, api.filters)
+	stateBlockNumber, hash, latest, err := rpchelper.GetBlockNumber(args.StateBlockNumberOrHash, tx, api.filters)
 	if err != nil {
 		return nil, err
 	}
@@ -92,34 +98,55 @@ func (api *APIImpl) CallBundle(ctx context.Context, txHashes []common.Hash, stat
 		return nil, fmt.Errorf("block %d(%x) not found", stateBlockNumber, hash)
 	}
 
-	blockNumber := stateBlockNumber + 1
+	blockNumber := big.NewInt(int64(args.BlockNumber))
 
-	timestamp := parent.Time // Dont care about the timestamp
-
+	timestamp := parent.Time + 1
+	if args.Timestamp != nil {
+		timestamp = *args.Timestamp
+	}
 	coinbase := parent.Coinbase
+	if args.Coinbase != nil {
+		coinbase = common.HexToAddress(*args.Coinbase)
+	}
+	difficulty := parent.Difficulty
+	if args.Difficulty != nil {
+		difficulty = args.Difficulty
+	}
+	gasLimit := parent.GasLimit
+	if args.GasLimit != nil {
+		gasLimit = *args.GasLimit
+	}
+	var baseFee *big.Int
+	if args.BaseFee != nil {
+		baseFee = args.BaseFee
+	} else if chainConfig.IsLondon(uint64(args.BlockNumber.Int64())) {
+		baseFee = misc.CalcBaseFee(chainConfig, parent)
+	}
+
 	header := &types.Header{
 		ParentHash: parent.Hash(),
-		Number:     big.NewInt(int64(blockNumber)),
-		GasLimit:   parent.GasLimit,
+		Number:     blockNumber,
+		GasLimit:   gasLimit,
 		Time:       timestamp,
-		Difficulty: parent.Difficulty,
+		Difficulty: difficulty,
 		Coinbase:   coinbase,
+		BaseFee:    baseFee,
 	}
 
 	// Get a new instance of the EVM
-	signer := types.MakeSigner(chainConfig, blockNumber)
-	rules := chainConfig.Rules(blockNumber)
+	signer := types.MakeSigner(chainConfig, blockNumber.Uint64())
+	rules := chainConfig.Rules(blockNumber.Uint64())
 	firstMsg, err := txs[0].AsMessage(*signer, nil, rules)
 	if err != nil {
 		return nil, err
 	}
 
-	blockCtx, txCtx := transactions.GetEvmContext(firstMsg, header, stateBlockNumberOrHash.RequireCanonical, tx, api._blockReader)
+	blockCtx, txCtx := transactions.GetEvmContext(firstMsg, header, args.StateBlockNumberOrHash.RequireCanonical, tx, api._blockReader)
 	evm := vm.NewEVM(blockCtx, txCtx, st, chainConfig, vm.Config{Debug: false})
 
 	timeoutMilliSeconds := int64(5000)
-	if timeoutMilliSecondsPtr != nil {
-		timeoutMilliSeconds = *timeoutMilliSecondsPtr
+	if args.Timeout != nil {
+		timeoutMilliSeconds = *args.Timeout
 	}
 	timeout := time.Millisecond * time.Duration(timeoutMilliSeconds)
 	// Setup context so it may be cancelled the call has completed
@@ -146,15 +173,24 @@ func (api *APIImpl) CallBundle(ctx context.Context, txHashes []common.Hash, stat
 	gp := new(core.GasPool).AddGas(math.MaxUint64)
 
 	results := []map[string]interface{}{}
+	blockState := state.New(stateReader)
+	if blockState == nil {
+		return nil, fmt.Errorf("can't get the current state")
+	}
+	coinbaseBalanceBefore := blockState.GetBalance(coinbase)
 
 	bundleHash := sha3.NewLegacyKeccak256()
+	var totalGasUsed uint64 = 0
+	gasFees := uint256.NewInt(0)
 	for _, txn := range txs {
+		coinbaseBalanceBeforeTx := blockState.GetBalance(coinbase)
 		msg, err := txn.AsMessage(*signer, nil, rules)
 		if err != nil {
 			return nil, err
 		}
 		// Execute the transaction message
 		result, err := core.ApplyMessage(evm, msg, gp, true /* refunds */, false /* gasBailout */)
+
 		if err != nil {
 			return nil, err
 		}
@@ -164,23 +200,58 @@ func (api *APIImpl) CallBundle(ctx context.Context, txHashes []common.Hash, stat
 		}
 
 		txHash := txn.Hash().String()
-		jsonResult := map[string]interface{}{
-			"txHash":  txHash,
-			"gasUsed": result.UsedGas,
+		from, err := txn.Sender(*signer)
+		if err != nil {
+			return nil, fmt.Errorf("err: %w; txhash %s", err, txn.Hash())
 		}
+		to := "0x"
+		if txn.GetTo() != nil {
+			to = txn.GetTo().String()
+		}
+		jsonResult := map[string]interface{}{
+			"txHash":      txHash,
+			"gasUsed":     result.UsedGas,
+			"fromAddress": from.String(),
+			"toAddress":   to,
+		}
+		totalGasUsed += result.UsedGas
+		gasPrice := txn.GetEffectiveGasTip(uint256.NewInt(header.BaseFee.Uint64()))
+		gasFeesTx := new(uint256.Int).Mul(uint256.NewInt(result.UsedGas), gasPrice)
+		gasFees.Add(gasFees, gasFeesTx)
+
 		bundleHash.Write(txn.Hash().Bytes())
 		if result.Err != nil {
 			jsonResult["error"] = result.Err.Error()
+			revert := result.Revert()
+			if len(revert) > 0 {
+				jsonResult["revert"] = string(revert)
+			}
 		} else {
-			jsonResult["value"] = common.BytesToHash(result.Return())
+			dst := make([]byte, hex.EncodedLen(len(result.Return())))
+			hex.Encode(dst, result.Return())
+			jsonResult["value"] = "0x" + string(dst)
 		}
 
+		coinbaseDiffTx := new(uint256.Int).Sub(blockState.GetBalance(coinbase), coinbaseBalanceBeforeTx)
+		jsonResult["coinbaseDiff"] = coinbaseDiffTx.String()
+		jsonResult["gasFees"] = gasFeesTx.String()
+		jsonResult["ethSentToCoinbase"] = new(uint256.Int).Sub(coinbaseDiffTx, gasFeesTx).String()
+		jsonResult["gasPrice"] = new(uint256.Int).Div(coinbaseDiffTx, uint256.NewInt(result.UsedGas)).String()
+		jsonResult["gasUsed"] = result.UsedGas
 		results = append(results, jsonResult)
 	}
 
 	ret := map[string]interface{}{}
 	ret["results"] = results
-	ret["bundleHash"] = hexutil.Encode(bundleHash.Sum(nil))
+	coinbaseDiff := new(uint256.Int).Sub(blockState.GetBalance(coinbase), coinbaseBalanceBefore)
+	ret["coinbaseDiff"] = coinbaseDiff.String()
+	ret["gasFees"] = gasFees.String()
+	ret["ethSentToCoinbase"] = new(uint256.Int).Sub(coinbaseDiff, gasFees).String()
+	ret["bundleGasPrice"] = new(uint256.Int).Div(coinbaseDiff, uint256.NewInt(totalGasUsed)).String()
+	ret["totalGasUsed"] = totalGasUsed
+	ret["stateBlockNumber"] = parent.Number.Int64()
+
+	ret["bundleHash"] = "0x" + common.Bytes2Hex(bundleHash.Sum(nil))
 	return ret, nil
 }
 
